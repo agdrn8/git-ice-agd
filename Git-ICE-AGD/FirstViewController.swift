9e7d949ad380e7c2e952e9695d42a7c103090516
Dieringer,Alexander G <s503330@ch140014m.nwmissouri.edu>
2016-11-14 13:23:22 -0600

    Initial Commit

---
+++ Git-ICE-AGD/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Git-ICE-AGD
+//
+//  Created by Dieringer,Alexander G on 11/14/16.
+//  Copyright © 2016 Dieringer,Alexander G. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Git-ICE-AGD/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Git-ICE-AGD
+//
+//  Created by Dieringer,Alexander G on 11/14/16.
+//  Copyright © 2016 Dieringer,Alexander G. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
