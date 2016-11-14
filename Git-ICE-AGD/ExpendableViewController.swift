e92aa995f1bbc625e6b424450f133633bbcaad4a
mprogers <mprogers@mac.com>
2016-11-14 13:48:54 -0600

    Add a button to BeeVC which Alex can then work on.

---
+++ Git-ICE-AGD/ExpendableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ExpendableViewController.swift
+//  Git-ICE-AGD
+//
+//  Created by Michael Rogers on 11/14/16.
+//  Copyright © 2016 Dieringer,Alexander G. All rights reserved.
+//
+
+import UIKit
+
+class ExpendableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
