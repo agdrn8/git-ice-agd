3e97355a5c785792c3c84c5ff05bc1acc2c1f865
Dieringer,Alexander G <s503330@ch140014m.nwmissouri.edu>
2016-11-18 13:31:31 -0600

    Removed text from BeeVC and EhVC and added an image view to EhVC

---
+++ Git-ICE-AGD/canvas.swift
@@ -0,0 +1,13 @@
+//
+//  canvas.swift
+//  Git-ICE-AGD
+//
+//  Created by Dieringer,Alexander G on 11/18/16.
+//  Copyright © 2016 Dieringer,Alexander G. All rights reserved.
+//
+
+import Foundation
+
+class Canvas {
+    
+}
